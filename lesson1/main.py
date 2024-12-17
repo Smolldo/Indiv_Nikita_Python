6ee91444352cddb2377a8cab18f3585ab4c9ad53
Mykhajlo Honyk <maximqwerbich@gmail.com>
2024-12-17 19:08:48 +0200

    урок1

---
+++ lesson1/main.py
@@ -0,0 +1,45 @@
+print('Hello world')
+
+a = 23
+
+b = 'John'
+
+print(a, b)
+
+print('Hello', b)
+
+print('Hello ' + b + ' ifosdfs')
+
+print(f'Hello {b}')
+
+# print('Hello' + 8) - ошибка
+
+
+# Вивести на екран повідомлення:"My name is..." (вписуємо своє ім'я)
+name = 'Misha'
+
+print('My name is ' + name)
+
+
+# Вивести на екран повідомлення:"My hobby is..." (вписуємо своє хобі)
+hobby = 'Computer games'
+
+print('My hobby is: ', hobby)
+
+# Вивести на екран повідомлення:"I decided to learn Python because..." (вписуємо чому ти вирішив вивчати Python)
+reason = 'of fun'
+
+print('I decided to learn Python because ', reason)
+
+
+
+# text = input('Enter some text')
+# print(text)
+
+# number = input('Enter some number: ')
+# print(int(number) + 5)
+
+name = input('Enter name: ')
+age = input('Enter age: ')
+
+print(f'My name is {name}. I am {age}')
